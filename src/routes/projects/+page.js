97247286662ab873acc596c2f9f06868dd62e0b1
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-03-14 10:36:23 +1300

    fix: update load function syntax and add type annotation

--- src/routes/projects/+page.js
+++ src/routes/projects/+page.js
@@ -1,8 +1,9 @@
-export async function load({ fetch }) {
+/** @type {import('./$types').PageLoad} */
+export const load = async ({ fetch }) => {
   const res = await fetch('/projects.json');
   const projects = await res.json();
 
   return {
     projects
   };
-}
+};
